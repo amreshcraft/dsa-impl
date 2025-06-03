f9967399bc8b8541a200dcb93ec5c6fb4b82251d
Amresh Maurya <amreshbinary@gmail.com>
2025-06-03 22:27:03 +0530

    initial commit

---
+++ automate.sh
@@ -0,0 +1,11 @@
+
+
+touch ./stack/MyStack.java ./array/MyArray.java dynamic-array/MyDynamicArray.java ./linked-list/MyLinkedList.java ./queue/MyQueue.java
+
+touch ./stack/MyStack.js ./array/MyArray.js dynamic-array/MyDynamicArray.js ./linked-list/MyLinkedList.js ./queue/MyQueue.js
+
+touch ./stack/MyStack.py ./array/MyArray.py dynamic-array/MyDynamicArray.py ./linked-list/MyLinkedList.py ./queue/MyQueue.py
+
+touch ./stack/MyStack.c ./array/MyArray.c dynamic-array/MyDynamicArray.c ./linked-list/MyLinkedList.c ./queue/MyQueue.c
+
+touch ./stack/MyStack.cpp ./array/MyArray.cpp dynamic-array/MyDynamicArray.cpp ./linked-list/MyLinkedList.cpp ./queue/MyQueue.cpp
