fc6c33f5ec1ee07f2032769413362e7117056370
Amresh Maurya <amreshbinary@gmail.com>
2025-06-14 15:05:28 +0530

    sll: add lots of methods for sll

--- linked-list/SinglyLinkedList.java
+++ linked-list/SinglyLinkedList.java
@@ -67,7 +67,39 @@ public class SinglyLinkedList {
     }
 
     // search node by given value
+    public boolean isFound(int value){
+        Node temp = this.start;
+        while(temp != null){
+            if(temp.item == value){
+                return true;
+            }
+            temp = temp.next;
+        }
+        return false;
+    }
     // insert a new node after a given node
+    public void insertAfterNode(int data, int afterThis) {
+    if (isEmpty()) {
+        System.out.println("List is empty. Cannot insert after " + afterThis);
+        return;
+    }
+
+    Node newNode = new Node(data);
+    Node temp = this.start;
+
+    while (temp != null) {
+        if (temp.item == afterThis) {
+            newNode.next = temp.next;
+            temp.next = newNode;
+            System.out.println("Inserted " + data + " after " + afterThis + " successfully.");
+            return;
+        }
+        temp = temp.next;
+    }
+
+    System.out.println("Node with value " + afterThis + " not found.");
+}
+
     // delete first node
     public void deleteFromBegin() {
         this.start = this.start.next;
@@ -89,7 +121,28 @@ public class SinglyLinkedList {
     }
 
     // delete a node with given item value
-    
+    public void deleteByValue(int value) {
+        Node temp = this.start;
+        if (isEmpty())
+            return;
+        if (temp.item == value) {
+            this.start = this.start.next;
+              System.out.println("Deleted node with value: " + value);
+            return;
+        }
+        while (temp.next != null) {
+            if (temp.next.item == value) {
+                temp.next = temp.next.next;
+                  System.out.println("Deleted node with value: " + value);
+                return;
+            }
+
+            temp = temp.next;
+
+        }
+    System.out.println("Node with value " + value + " not found.");
+    }
+
     // display
     public void display(Node start) {
         while (start != null) {
@@ -103,6 +156,9 @@ public class SinglyLinkedList {
     public static void main(String[] args) {
         SinglyLinkedList sll = new SinglyLinkedList();
         sll.insertAtLast(10);
+        sll.insertAtLast(20);
+        sll.insertAtLast(30);
+        sll.insertAtLast(40);
         System.out.println("insert at last");
         sll.display(sll.start);
         sll.insertAtBegin(60);
@@ -114,6 +170,11 @@ public class SinglyLinkedList {
         System.out.println("delete from first");
         sll.deleteFromBegin();
         sll.display(sll.start);
-
+        System.out.println("delete by value:");
+        sll.deleteByValue(10);
+        sll.display(sll.start);
+        System.out.println("insert after by value: ");
+        sll.insertAfterNode(101,20);
+        sll.display(sll.start);
     }
 }
